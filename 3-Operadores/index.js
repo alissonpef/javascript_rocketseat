76e9dfcaa1cfa7c73b6545817028bcf2cf1546a5
Alisson Pereira <alissonpef@gmail.com>
2025-09-02 17:17:17 -0300

    Add 3-Operadores

---
+++ 3-Operadores/index.js
@@ -0,0 +1,27 @@
+console.log("Soma: ", 12 + 2);
+console.log("subtração: ", 12 - 2);
+console.log("Multiplicação: ", 12 * 2);
+console.log("Divisão: ", 12 / 2);
+console.log("Resto da divisão: ", 12 / 2);
+console.log("Exponencial: ", 12 ** 2);
+
+let number = 10;
+number += 10;
+number -= 10;
+number *= 1;
+number /= 1;
+number %= 1;
+number **= 1;
+
+console.log("Incremento após: ", number++);
+console.log("Incremento antes: ", ++number);
+console.log("Decremento após: ", number--);
+console.log("Decremento antes: ", --number);
+
+const one = 1;
+const oneString = "1";
+
+console.log('1 é igual a "1" usando == ?', one == oneString);
+console.log('1 é estritamente igual a "1" usando === ?', one === oneString);
+console.log('1 é diferente a "1" usando != ?', one != oneString);
+console.log('1 é estritamente diferente a "1" usando !== ?', one !== oneString);
