715a3b562e5e0a5217b5dd3402e1e1851bd1d6c7
Alisson Pereira <alissonpef@gmail.com>
2025-09-02 10:46:46 -0300

    Add 1-Variaveis

---
+++ 1-Variáveis/index.js
@@ -0,0 +1,23 @@
+let userLet;
+userLet = "Alisson";
+console.log(userLet);
+userLet = "Teste";
+console.log(userLet);
+
+var userVar;
+userVar = "Alisson";
+console.log(userVar);
+userVar = "Teste";
+console.log(userVar);
+
+const userConst = "Alisson";
+console.log(userConst);
+
+teste = "Testando o Hoisting";
+console.log(teste);
+var test;
+
+{
+  teste = "Testando o Escopo do Var";
+}
+console.log(teste);
