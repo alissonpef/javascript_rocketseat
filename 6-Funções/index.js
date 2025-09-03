7586780746065c8cedb8b37d070b44bea99ae0b7
Alisson Pereira <alissonpef@gmail.com>
2025-09-03 09:47:59 -0300

    Add 6-Funções

---
+++ 6-Funções/index.js
@@ -0,0 +1,39 @@
+function message(username) {
+  console.log("Olá", username);
+}
+
+message("Alisson");
+
+/**
+ *
+ * @param {*} a
+ * @param {*} b
+ * @returns
+ */
+function sum(a, b) {
+  let result = a + b;
+  return result;
+}
+
+console.log(sum(1, 2));
+
+// Função anônima
+const anonymousFunction = function (name) {
+  return "Olá, " + name;
+};
+
+console.log(anonymousFunction("Alisson"));
+
+const arrowFunction = (name) => {
+  return "Olá, " + name;
+};
+
+console.log(arrowFunction("Alisson"));
+
+function callbackFunction(taskName, callback) {
+  console, log("Executando a tarefa: ", taskName);
+}
+
+execute("Download do arquivo...", () => {
+  console.log("Tarefa finalizada.");
+});
