5c97aabfb3afc60890f083baedd72a157a47fd9c
Alisson Pereira <alissonpef@gmail.com>
2025-09-04 10:29:36 -0300

    Add 7-Objetos

---
+++ 7-Objetos/index.js
@@ -0,0 +1,28 @@
+const user = {
+  email: "alisson@email.com",
+  age: 23,
+  name: {
+    first_name: "Alisson",
+    surname: "Pereira",
+  },
+  addres: {
+    street: "Rua X",
+    number: 1234,
+    city: "Santa Catarina",
+  },
+
+  messageAddres: () => {
+    console.log(user.addres.city);
+  },
+
+  messageUser: function () {
+    console.log(`Olá ${this.name.first_name}`);
+  },
+};
+
+user.messageAddres();
+user["messageUser"]();
+user.name.first_name = "Vitoria";
+user.messageUser();
+
+console.log(user?.description);
